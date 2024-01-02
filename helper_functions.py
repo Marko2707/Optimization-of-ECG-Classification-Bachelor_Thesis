8a5d36e77a27a75fdb7bff9d40147a6e9074df7d
Marko Stankovic <marko.stankovic272001@gmail.com>
2024-01-02 19:44:52 +0100

    Comments

--- helper_functions.py
+++ helper_functions.py
@@ -11,7 +11,9 @@ import os
 
 def create_folder_if_not_exists(folder_name):
     if not os.path.exists(folder_name):
+        #if folder does not exist, creates it
         os.makedirs(folder_name)
         print(f"Folder '{folder_name}' created successfully.")
     else:
+        #already created
         print(f"Folder '{folder_name}' already exists.")
